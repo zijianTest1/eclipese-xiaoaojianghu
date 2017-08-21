c55d18d2f6b51fff07bef79b88ca72150b64d6b1
admin2 <571264532@qq.com>
2017-08-21 20:16:15 +0800

    aaa

--- src/com/atguigu/xiaoaojianghu/Miji.java
+++ src/com/atguigu/xiaoaojianghu/Miji.java
@@ -5,9 +5,7 @@ public class Miji {
 			public static void main(String[] args) {
 				
 				System.out.println("辟邪剑法！");
-				System.out.println("独孤九剑！");
-				System.out.println("葵花宝典！");
-				System.out.println("吸星大法！");
+				System.out.println("辟邪剑法！");
 			}
 	
 }
